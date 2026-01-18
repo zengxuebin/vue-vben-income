420e9f0ace067d74a673b69fe80688dec28cdde6
zengxuebin <1812806483@qq.com>
2026-01-18 20:21:29 +0800

    fix：启动报错

--- packages/effects/layouts/src/widgets/index.ts
+++ packages/effects/layouts/src/widgets/index.ts
@@ -8,7 +8,6 @@ export { default as AuthenticationLayoutToggle } from './layout-toggle.vue';
 export * from './lock-screen';
 export * from './notification';
 export * from './preferences';
-export * from './tenant-dropdown';
 export * from './theme-toggle';
 export * from './timezone';
 export * from './user-dropdown';
